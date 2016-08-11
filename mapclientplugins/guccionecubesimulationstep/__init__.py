3b97052dfe5950d92001121115d883a457d05dcb
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-08-11 14:26:16 +1200

    Set location of step source.

--- mapclientplugins/guccionecubesimulationstep/__init__.py
+++ mapclientplugins/guccionecubesimulationstep/__init__.py
@@ -6,7 +6,7 @@ MAP Client Plugin
 __version__ = '0.1.0'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Guccione Cube Simulation'
-__location__ = ''
+__location__ = 'https://github.com/mapclient-plugins/guccionecubesimulation/archive/v0.1.0.zip'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.guccionecubesimulationstep import step
