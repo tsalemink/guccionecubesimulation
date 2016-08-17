fba81ad53271dbc33bd7c06c9ceabfb68b3e6d5d
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-08-18 10:26:16 +1200

    Update version to v0.1.1.

--- mapclientplugins/guccionecubesimulationstep/__init__.py
+++ mapclientplugins/guccionecubesimulationstep/__init__.py
@@ -3,10 +3,10 @@
 MAP Client Plugin
 '''
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Guccione Cube Simulation'
-__location__ = 'https://github.com/mapclient-plugins/guccionecubesimulation/archive/v0.1.0.zip'
+__location__ = 'https://github.com/mapclient-plugins/guccionecubesimulation/archive/v0.1.1.zip'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.guccionecubesimulationstep import step
